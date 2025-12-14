9f2ed73bd19b82de3cddf4661931d1a6370a757a
Beau LaWalt <beaulawalt@icloud.com>
2025-12-14 13:09:20 -0500

    Removed Time Stuff

--- Day1/Part2/Day1P2.py
+++ Day1/Part2/Day1P2.py
@@ -1,6 +1,3 @@
-import time
-start_time = time.time()
-
 startingPt, key_to_door = 50,0
 with open("input.txt","r") as f:
     for i in f:
@@ -12,8 +9,6 @@ with open("input.txt","r") as f:
         crossings = abs(target // 100 - startingPt // 100)
         key_to_door += crossings
         startingPt = target
-end_time = time.time()
+
 print(key_to_door)
 print(startingPt)
-print(f"Time: {(end_time - start_time) * 1000:.2f} ms")
-
